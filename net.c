896c83643f0fb67c5a3a7ecc69356dde7f68a5e7
hikaru-champion <hikaru.chanp0728@gmail.com>
2023-08-12 22:38:25 +0000

    add func net_device_register

--- net.c
+++ net.c
@@ -28,6 +28,14 @@ net_device_alloc(void)
 int
 net_device_register(struct net_device *dev)
 {
+	static unsigned int index = 0;
+	
+	dev->index = index++;
+	snprintf(dev->name, sizeof(dev->name), "net%d", dev->index);
+	dev->next = devices;
+	devices = dev;
+	infof("registered, dev=%s, type=0x%@4x", dev->name, dev->type);
+	return 0;
 }
 
 static int
